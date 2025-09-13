14ec355d6a9ee81a050752db963f19742f897e9d
Prakash <pgcet601@gmail.com>
2025-09-13 06:48:26 +0000

    fix this issue asap

--- next.config.ts
+++ next.config.ts
@@ -32,11 +32,13 @@ const nextConfig: NextConfig = {
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
+      // Don't resolve 'async_hooks' on the client
       config.resolve.fallback = {
         ...config.resolve.fallback,
         async_hooks: false,
       };
     }
+
     return config;
   },
 };

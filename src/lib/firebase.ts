986afaa9ace962e1776a80f6947b5b0114aafd3e
Prakash <pgcet601@gmail.com>
2025-09-13 08:57:40 +0000

    make all this in env file please : const firebaseConfig = {   apiKey: "A

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,12 +8,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDXbwDTCTpjlEM4rKSloTWmXvGuy57fGKM",
-  authDomain: "studio-2794072808-255ab.firebaseapp.com",
-  projectId: "studio-2794072808-255ab",
-  storageBucket: "studio-2794072808-255ab.firebasestorage.app",
-  messagingSenderId: "625308181510",
-  appId: "1:625308181510:web:6e72803905e41a9b238f87"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase

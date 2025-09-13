5fdd54aad5fe446e8a6ca5f3e546f0afd73c46a9
Prakash <pgcet601@gmail.com>
2025-09-13 09:27:57 +0000

    FirebaseError: Firebase: Error (auth/popup-closed-by-user).
     ????

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -62,7 +62,8 @@ export default function LoginPage() {
         <CardContent>
           <div className="flex flex-col gap-4">
             <Button variant="outline" className="w-full" onClick={handleGoogleLogin} disabled={isSigningIn}>
-              {isSigningIn ? <Loader2 className="h-4 w-4 animate-spin" /> : <span className="ml-2">Login with Google</span>}
+              {isSigningIn ? <Loader2 className="h-4 w-4 animate-spin" /> : <svg className="w-4 h-4 mr-2" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 381.5 512 244 512 109.8 512 0 402.2 0 256S109.8 0 244 0c73.2 0 136 25.3 186.3 66.3l-67.5 64.9C298.7 100.2 272.7 88 244 88c-85.3 0-153.9 68.7-153.9 168s68.7 168 153.9 168c92.1 0 135.3-63.5 139.9-98.4H244v-75h243.9c1.3 12.2 2.1 25.1 2.1 38.8z"></path></svg>}
+              {isSigningIn ? "Signing In..." : "Login with Google"}
             </Button>
           </div>
         </CardContent>
